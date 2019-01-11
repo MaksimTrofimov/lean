cf4571901828e7c05ec993b484163759abb1678a
Maksim Trofimov <maksim.t.kh@gmail.com>
2019-01-12 01:44:40 +0200

    page about. slider complete

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    $(document).ready(function(){
-        $('.owl-carousel').owlCarousel({
-            loop:true,
-            margin:0,
-            nav:true,
-            navText: ['', ''],
-            responsive:{
-                0:{
-                    items:1
-                },
-            }
-        })
-    });
-
-});
+// $(document).ready(function(){
+//     $(document).ready(function(){
+//         $('.owl-carousel').owlCarousel({
+//             loop:true,
+//             margin:0,
+//             nav:true,
+//             navText: ['', ''],
+//             responsive:{
+//                 0:{
+//                     items:1
+//                 },
+//             }
+//         })
+//     });
+//
+// });
 
 
 
